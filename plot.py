4a84fbb51fcecf57997b97e5f63c63634f268cdc
Jacob Kvasnicka <jacob.kvasnicka@mail.utoronto.ca>
2023-08-28 10:36:16 -0500

    Create function: plot.feature_distributions

--- plot.py
+++ plot.py
@@ -19,6 +19,85 @@ _flierprops = dict(
     markeredgecolor='lightgray'
     )
 
+#region: feature_distributions
+def feature_distributions(
+        data_manager, 
+        features_source='opera', 
+        ld50_type='predicted', 
+        data_condition='missing',
+        color='cornflowerblue'
+        ):
+    '''
+    '''
+    X = data_manager.load_features(
+        features_source=features_source, 
+        ld50_type=ld50_type, 
+        data_condition=data_condition
+    )
+
+    discrete_features = [name for name in X if 'discrete' in name]
+    continuous_features = list(X.columns.difference(set(discrete_features)))
+
+    # Determine the number of columns and calculate the number of rows
+    ncols = 5
+    n_features = len(continuous_features) + len(discrete_features)
+    nrows = (n_features + ncols - 1) // ncols
+
+    # Determine the size of each subplot and calculate the total figure size
+    subplot_width = 2.5 
+    subplot_height = 2  
+    figsize = (ncols * subplot_width, nrows * subplot_height)
+
+    # Set up the subplots
+    fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
+
+    # Plot continuous features
+    for i, feature in enumerate(continuous_features):
+        row = i // 5
+        col = i % 5
+        # Use the square-root rule
+        number_of_bins = int(np.sqrt(len(X[feature].dropna())))
+        axs[row, col].hist(
+            X[feature].dropna(), 
+            color=color, 
+            bins=number_of_bins, 
+            edgecolor=color, 
+            linewidth=0.5
+        )
+        axs[row, col].set_title(feature)
+
+    # Plot discrete features
+    for i, feature in enumerate(discrete_features):
+        non_null_values = X[feature].dropna()
+        unique_values = non_null_values.unique()
+        # Check if there are any non-null values
+        if len(unique_values) > 0:
+            row = (i + len(continuous_features)) // 5
+            col = (i + len(continuous_features)) % 5
+            value_counts = non_null_values.value_counts().sort_index()
+            axs[row, col].bar(
+                value_counts.index, 
+                value_counts.values, 
+                color=color, 
+                edgecolor=color
+            )
+            axs[row, col].set_title(feature)
+            
+            if len(unique_values) <= 2:
+                axs[row, col].set_xticks(unique_values)
+                integer_labels = [int(value) for value in unique_values]
+                axs[row, col].set_xticklabels(integer_labels)
+
+    # Remove unused subplots
+    total_features = len(continuous_features) + len(discrete_features)
+    for i in range(total_features, nrows * ncols):
+        axs.flatten()[i].axis('off')
+
+    fig.tight_layout()
+
+    save_figure(fig, feature_distributions, 'feature-distributions')
+#endregion
+
 #region: pairwise_scatters_and_kde_subplots
 def pairwise_scatters_and_kde_subplots(
         features_file, 
