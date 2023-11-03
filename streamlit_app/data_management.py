584f1996cda5a30aa7fae4c5e37c1293cf2d65f4
Jacob Kvasnicka <jacob.kvasnicka@mail.utoronto.ca>
2023-11-03 14:24:17 -0500

    Incorporate data management for MOEs

--- streamlit_app/data_management.py
+++ streamlit_app/data_management.py
@@ -28,25 +28,10 @@ class DataManager:
 
     #region: effect 
     @property
-    def effect_label(self):
+    def effect(self):
         '''
         '''
-        return self._effect_label
-    
-    @effect_label.setter
-    def effect_label(self, new_effect_label):
-        '''
-        '''
-        self._effect_label =  new_effect_label
-
-        if new_effect_label:
-            # Get the corresponding key for data lookups
-            self._effect = self._config['effect_for_label'][new_effect_label]
-            # Append the effect name to the data directory
-            self._effect_subdir = os.path.join(
-                self._config['data_dir'], 
-                self._effect
-            )
+        return self._config['effect_for_label'][self.effect_label]
     #endregion
 
     #region: chemical_id 
@@ -70,10 +55,11 @@ class DataManager:
     def get_qsar_ready_smiles(self, chemical_id):
         '''
         '''
-        smiles_for_id = load_qsar_ready_smiles(
-            self._config['data_dir'], 
-            self._config['chem_ids_file_name']
-            )
+        smiles_file = self.build_data_path(
+            'chem_ids_file_name',
+            is_effect_specific=False
+        )
+        smiles_for_id = load_qsar_ready_smiles(smiles_file)
         
         return smiles_for_id[chemical_id]
     #endregion
@@ -83,37 +69,55 @@ class DataManager:
     def features(self):
         '''
         '''
-        X = load_features(
-            self._effect_subdir,
-            self._config['features_file_name']
-        )
+        features_file = self.build_data_path('features_file_name')
+        X = load_features(features_file)
 
         return X.loc[self.chemical_id]
     #endregion
 
+    # TODO: Change to 'points_of_departure'
     #region: point_of_departure
     @property
     def point_of_departure(self):
         '''
         '''
-        pod_data = load_points_of_departure(
-            self._effect_subdir,
-            self._config['pod_file_name']
-        )
+        pod_file = self.build_data_path('pod_file_name')
+        pod_data = load_points_of_departure(pod_file)
 
         return pod_data.loc[self.chemical_id]
     #endregion
 
+    #region: margins_of_exposure
+    @property
+    def margins_of_exposure(self):
+        '''
+        '''
+        moe_file = self.build_data_path('moe_file_name')
+        moe_data = load_margins_of_exposure(moe_file)
+
+        return moe_data.loc[self.chemical_id]
+    #endregion
+
+    #region: build_data_path
+    def build_data_path(self, file_key, is_effect_specific=True):
+        '''
+        '''
+        file_name = self._config[file_key]
+
+        data_dir = self._config['data_dir']
+        if is_effect_specific:
+            data_dir = os.path.join(data_dir, self.effect)
+        
+        return os.path.join(data_dir, file_name)
+    #endregion
+
 #region: load_qsar_ready_smiles
 @st.cache_data
-def load_qsar_ready_smiles(effect_subdir, file_name):
+def load_qsar_ready_smiles(smiles_file):
     '''
     '''
     smiles_for_id = (
-        read_parquet(
-            effect_subdir,
-            file_name
-        )
+        read_data(smiles_file)
         .squeeze()
         .to_dict()
     )
@@ -123,24 +127,36 @@ def load_qsar_ready_smiles(effect_subdir, file_name):
 
 #region: load_features
 @st.cache_data
-def load_features(effect_subdir, file_name):
+def load_features(features_file):
     '''
     '''
-    return read_parquet(effect_subdir, file_name)
+    return read_data(features_file)
 #endregion
 
 #region: load_points_of_departure
 @st.cache_data
-def load_points_of_departure(effect_subdir, file_name):
+def load_points_of_departure(pod_file):
     '''
     '''
-    return read_parquet(effect_subdir, file_name)
+    return read_data(pod_file)
 #endregion
 
-#region: read_parquet
-def read_parquet(effect_subdir, file_name):
+#region: load_margins_of_exposure
+@st.cache_data
+def load_margins_of_exposure(moe_file):
     '''
     '''
-    full_path = os.path.join(effect_subdir, file_name)
-    return pd.read_parquet(full_path)
+    return read_data(moe_file)
+#endregion
+
+#region: read_data
+def read_data(data_file):
+    '''
+    '''
+    extension = data_file.split('.')[-1]
+
+    if extension == 'parquet':
+        return pd.read_parquet(data_file)
+    else:
+        raise ValueError(f'File extension not accepted: {extension}')
 #endregion
