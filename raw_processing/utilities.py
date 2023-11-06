62954559d43c370b8f8aa00b27dd6f5d25e96774
Jacob Kvasnicka <jacob.kvasnicka@mail.utoronto.ca>
2023-11-06 09:16:17 -0600

    Use helper functions: inverse_log10_transform

--- raw_processing/utilities.py
+++ raw_processing/utilities.py
@@ -15,11 +15,31 @@ def inverse_log10_transform(data, log10_pat):
     '''
     data = data.copy()
 
-    log_columns = [c for c in data if log10_pat in c]
+    log_columns = get_columns_with_pattern(data.columns, log10_pat)
     if not log_columns:
         raise ValueError(
             f'log10 pattern, "{log10_pat}," not detected in any columns')
-    data.loc[:, log_columns] = 10**data[log_columns]
+    
+    data = apply_inverse_log_transform(data, log_columns)
+    
     data.columns = data.columns.str.replace(log10_pat, '')
+    return data
+#endregion
+
+#region: get_columns_with_pattern
+def get_columns_with_pattern(columns, pattern):
+    '''
+    Helper function to get all columns that contain the specified pattern.
+    '''
+    return [col for col in columns if pattern in col]
+#endregion
+
+#region: apply_inverse_log_transform
+def apply_inverse_log_transform(data, columns):
+    '''
+    Helper function to apply inverse log10 transformation to specified 
+    columns.
+    '''
+    data[columns] = 10**data[columns]
     return data
 #endregion
