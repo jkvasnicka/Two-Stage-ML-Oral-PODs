dbe9efaea8e55c9800abc9d12e84e28b2c65dfe1
Jacob Kvasnicka <jacob.kvasnicka@mail.utoronto.ca>
2023-11-08 08:48:15 -0600

    Store user inputs in a dict for batch download

--- streamlit_app/pages/2_Batch_Download.py
+++ streamlit_app/pages/2_Batch_Download.py
@@ -17,11 +17,10 @@ def main():
 
     with st.sidebar:
         st.header('User Input')
-
         effect_labels = dm.get_effect_labels(config)
-        effect_label, pod_selected, moe_selected, features_selected = get_user_inputs(effect_labels)
+        inputs = get_user_inputs(effect_labels)
 
-    prepare_data_download(config, effect_label, pod_selected, moe_selected, features_selected) 
+    prepare_data_download(inputs, config) 
 #endregion
 
 #region: initialize_page
@@ -42,32 +41,32 @@ def initialize_page(config):
     )
 #endregion
 
-# TODO: Return a dict?
 #region: get_user_inputs
 def get_user_inputs(effect_labels):
     '''
     '''
-    effect_label = st.selectbox(
+    inputs = {}  # initialize
+
+    inputs['effect_label'] = st.selectbox(
         'Select the effect category',
         options=effect_labels
     )
 
-    pod_selected = st.checkbox('Points of Departure')
-    moe_selected = st.checkbox('Margins of Exposure')
-    features_selected = st.checkbox('Features (OPERA 2.9)')
+    inputs['pod_selected'] = st.checkbox('Points of Departure')
+    inputs['moe_selected'] = st.checkbox('Margins of Exposure')
+    inputs['features_selected'] = st.checkbox('Features (OPERA 2.9)')
 
-    return effect_label, pod_selected, moe_selected, features_selected
+    return inputs
 #endregion
 
 #region: prepare_data_download
-def prepare_data_download(
-        config, effect_label, pod_selected, moe_selected, features_selected):
+def prepare_data_download(inputs, config):
     '''
     '''
-    zip_buffer = create_downloadable_zip_file(
-        config, effect_label, pod_selected, moe_selected, features_selected)
+    zip_buffer = create_downloadable_zip_file(inputs, config)
 
-    if any([pod_selected, moe_selected, features_selected]):
+    selected_inputs = [v for k, v in inputs.items() if 'selected' in k]
+    if any(selected_inputs):
         # Offer the zip file for download
         st.download_button(
             label='Download Selected Datasets',
@@ -80,10 +79,14 @@ def prepare_data_download(
 #endregion
 
 #region: create_downloadable_zip_file
-def create_downloadable_zip_file(
-        config, effect_label, pod_selected, moe_selected, features_selected):
+def create_downloadable_zip_file(inputs, config):
     '''
     '''
+    effect_label = inputs['effect_label']
+    pod_selected = inputs['pod_selected']
+    moe_selected = inputs['moe_selected']
+    features_selected = inputs['features_selected']
+
     # Create a zip file in memory
     zip_buffer = BytesIO()
     with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED) as zip_file:
